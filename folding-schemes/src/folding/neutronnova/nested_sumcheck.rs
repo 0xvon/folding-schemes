2b65286fa0a8ae0e6678ff0ce36047e617426467
masatsch <kingmasatojames@gmail.com>
2024-11-04 11:24:45 +0900

    gen e1 and e2

--- folding-schemes/src/folding/neutronnova/nested_sumcheck.rs
+++ folding-schemes/src/folding/neutronnova/nested_sumcheck.rs
@@ -1,63 +1,57 @@
 use crate::folding::neutronnova::zerocheck::ZeroCheckInstance;
 use ark_ff::PrimeField;
 use ark_poly::{DenseMultilinearExtension, MultilinearExtension};
+use ark_std::rand::Rng;
 use ark_std::sync::Arc;
+use ark_std::test_rng;
 
 pub struct NestedSumCheckInstance<F: PrimeField> {
     pub G: Vec<Arc<DenseMultilinearExtension<F>>>,
     pub F_poly: Vec<(F, Vec<usize>)>,
     pub z: Vec<F>,
     pub tau: F,
-    pub e: DenseMultilinearExtension<F>,
+    pub e1: DenseMultilinearExtension<F>,
+    pub e2: DenseMultilinearExtension<F>,
 }
 
 impl<F: PrimeField> From<ZeroCheckInstance<F>> for NestedSumCheckInstance<F> {
     fn from(zc: ZeroCheckInstance<F>) -> Self {
         // Step 1: Generate a random challenge tau
         // TODO: Use a proper RNG
-        let mut rng = ark_std::test_rng();
-        let tau = F::rand(&mut rng);
+        let mut rng = test_rng();
+
+        let tau_int = rng.gen::<u8>() as usize;
+        let tau = F::from(tau_int as u64);
 
         // Step 2: Compute Te as a power of tau
-        let mut evaluations = vec![];
-        let l = zc.F_poly.len(); // Assuming l is the length of z
+        let l = 1 << zc.F_poly.len();
+        let m = 1 << l;
 
-        for x in 0..(1 << l) {
-            let mut eq_sum = F::zero();
-            let x_bits: Vec<bool> = (0..l).map(|i| (x >> i) & 1 == 1).collect();
+        let sqrt_m_usize = (m as f64).sqrt() as usize; // Convert to usize for loop control
 
-            for z in 0..(1 << l) {
-                let z_bits: Vec<bool> = (0..l).map(|i| (z >> i) & 1 == 1).collect();
+        let mut e1_values = Vec::with_capacity(sqrt_m_usize);
+        let mut e2_values = Vec::with_capacity(sqrt_m_usize);
 
-                // Compute eq(z, x) using the equality polynomial eq(z, x)
-                let eq_z_x = x_bits
-                    .iter()
-                    .zip(z_bits.iter())
-                    .fold(F::one(), |acc, (&xi, &zi)| {
-                        let xi_f = if xi { F::one() } else { F::zero() };
-                        let zi_f = if zi { F::one() } else { F::zero() };
-                        acc * (xi_f * zi_f + (F::one() - xi_f) * (F::one() - zi_f))
-                    });
+        // Compute e1 as τ^0, τ^1, ..., τ^(sqrt(m)-1)
+        // Compute e2 as τ^0, τ^sqrt(m), ..., τ^{(sqrt(m)-1) *sqrt(m)}
+        for i in 0..sqrt_m_usize {
+            let power_of_tau_e1 = tau_int.pow(i as u32);
+            e1_values.push(F::from(power_of_tau_e1 as u64));
 
-                // Compute τ^Z, where Z is the decimal representation of z
-                let z_decimal = z as u64;
-                let tau_z = tau.pow([z_decimal]);
-
-                // Accumulate the value of eq(z, x) * τ^Z
-                eq_sum += eq_z_x * tau_z;
-            }
-            evaluations.push(eq_sum);
+            let power_of_tau_e2 = power_of_tau_e1 * tau_int;
+            e2_values.push(F::from(power_of_tau_e2 as u64));
         }
 
-        // Create e as a DenseMultilinearExtension based on the computed evaluations
-        let e = DenseMultilinearExtension::from_evaluations_vec(l, evaluations);
+        let e1 = DenseMultilinearExtension::from_evaluations_vec(zc.F_poly.len(), e1_values);
+        let e2 = DenseMultilinearExtension::from_evaluations_vec(zc.F_poly.len(), e2_values);
 
         Self {
             G: zc.G,
             F_poly: zc.F_poly,
             z: zc.z,
             tau,
-            e,
+            e1,
+            e2,
         }
     }
 }
@@ -75,7 +69,7 @@ impl<F: PrimeField> NestedSumCheckInstance<F> {
                 .collect();
 
             let q_x = self.evaluate_q(&x_f);
-            T += self.e.evaluate(&x_f).unwrap() * q_x;
+            T += self.e1.evaluate(&x_f).unwrap() * self.e2.evaluate(&x_f).unwrap() * q_x;
         }
         T
     }
@@ -102,6 +96,57 @@ impl<F: PrimeField> NestedSumCheckInstance<F> {
     // }
 }
 
+// pub struct PowerCheckInstance<F: PrimeField> {
+//     pub F_poly: Vec<(F, Vec<usize>)>,
+//     pub e1: DenseMultilinearExtension<F>,
+//     pub e2: DenseMultilinearExtension<F>,
+//     pub g2: DenseMultilinearExtension<F>,
+//     pub g3: DenseMultilinearExtension<F>,
+// }
+
+// impl<F: PrimeField> From<NestedSumCheckInstance<F>> for PowerCheckInstance<F> {
+//     fn from(nsc: NestedSumCheckInstance<F>) -> Self {
+//         let sqrt_m = nsc.e1.evaluations.len();
+
+//         // Compute g2 based on Construction 2
+//         let mut g2_values = vec![F::one()]; // g2(0) = 1
+//         g2_values.extend_from_slice(&nsc.e1.evaluations[..sqrt_m - 1]); // g2(i) = e_(i-1) for 1 <= i < sqrt(m)
+//         g2_values.push(F::one()); // g2(sqrt(m)) = 1
+//         g2_values.push(nsc.e1.evaluations[sqrt_m - 2]); // g2(sqrt(m) + 1) = e_(sqrt(m) - 2)
+//         g2_values.push(nsc.e1.evaluations[sqrt_m - 1]); // g2(sqrt(m) + 2) = e_(sqrt(m) - 1)
+//         g2_values.push(nsc.e2.evaluations[0]); // g2(sqrt(m) + 3) = e_sqrt(m+1)
+
+//         // Convert g2 to a DenseMultilinearExtension
+//         let g2 = DenseMultilinearExtension::from_evaluations_vec(sqrt_m, g2_values);
+
+//         // Compute g3 based on Construction 2
+//         let mut g3_values = vec![F::one()]; // g3(0) = 1
+//         for i in 1..sqrt_m {
+//             g3_values.push(nsc.tau.pow([i as u64])); // g3(i) = tau^i for 1 <= i < sqrt(m)
+//         }
+//         g3_values.push(F::one()); // g3(sqrt(m)) = 1
+//         g3_values.push(nsc.tau); // g3(sqrt(m) + 1) = tau
+//         g3_values.push(nsc.e1.evaluations[sqrt_m - 1]); // g3(sqrt(m) + 2) = e_(sqrt(m) - 1)
+//         g3_values.extend(nsc.e1.evaluations.iter().skip(sqrt_m - 2).take(2).cloned()); // g3(sqrt(m) + 3 and 4) = e_(sqrt(m) - 2), e_(sqrt(m) - 1)
+
+//         // Convert g3 to a DenseMultilinearExtension
+//         let g3 = DenseMultilinearExtension::from_evaluations_vec(sqrt_m, g3_values);
+
+//         let F_poly = vec![
+//             (F::one(), vec![0]),          // Represents y1 with coefficient +1
+//             (F::one().neg(), vec![1, 2]), // Represents -y2 * y3
+//         ];
+
+//         Self {
+//             F_poly,
+//             e1: nsc.e1,
+//             e2: nsc.e2,
+//             g2,
+//             g3,
+//         }
+//     }
+// }
+
 #[cfg(test)]
 pub mod tests {
     use super::*;
@@ -111,7 +156,7 @@ pub mod tests {
     use ark_std::Zero;
 
     #[test]
-    fn test_eval_nestedsumcheck_from_css() {
+    fn test_eval_nestedsumcheck_from_ccs() {
         let ccs = get_test_ccs::<Fr>();
         let z = get_test_z::<Fr>(3);
 
